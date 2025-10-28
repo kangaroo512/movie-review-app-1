be9cf1f8410c3b26f013be44367f4ee32806dcfd
Louded Leopard <pihir45318@iotrama.com>
2025-10-28 12:27:54 -0500

    Add Dockerfile for Render Deployment and Changed the name of csproj file

--- Movie Review API/obj/Debug/net8.0/Movie Review API.AssemblyInfo.cs
+++ Movie Review API/obj/Debug/net8.0/Movie Review API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Movie Review API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13116c3414e7e5678b455fcb365e0f5f9fd322db")]
 [assembly: System.Reflection.AssemblyProductAttribute("Movie Review API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Movie Review API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
